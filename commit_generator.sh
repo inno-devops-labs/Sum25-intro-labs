eb1f95a74781f346e337a0f40cec80693bfafca6
dsomni <pro100pro10010@gmail.com>
2025-06-09 20:03:03 +0300

    Add [commit generator] global

---
+++ commit_generator.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <number_of_iterations>"
+    exit 1
+fi
+
+if ! [[ "$1" =~ ^[0-9]+$ ]]; then
+    echo "Error: Parameter must be a positive integer"
+    exit 1
+fi
+
+
+touch random.txt
+
+for ((i=1; i<=$1; i++)); do
+    random_str=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
+
+    echo "$random_str" >> random.txt
+
+    git add random.txt
+    git commit -m "Add [$random_str] random.txt"
+
+    echo "Added and committed: $random_str"
+done
+
+echo "Done! $1 random strings added and committed."
